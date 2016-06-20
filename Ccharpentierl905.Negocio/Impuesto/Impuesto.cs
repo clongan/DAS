2b293b50dd6107810de124a1c4f7b9de058ed63e
Chris <Chris@KAREN-PC>
2016-06-19 23:59:21 -0600

    CREATED IMPUESTO PROCEDIMIENTO

---
+++ Ccharpentierl905.Negocio/Impuesto/Impuesto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Ccharpentierl905.Negocio.ComoUnProcedimiento.Impuesto
+{
+    public class Impuesto
+    {
+        public static double DeterminarImpuesto(double valorFacial, double valorTransadoNeto, double tasaDeImpuesto,
+            DateTime fechaDeVencimiento, DateTime fechaActual, Boolean tieneTratamientoFiscal, double rendimientoPorDescuento)
+        {
+            double impuesto = 0;
+
+            if (tieneTratamientoFiscal)
+            {
+                double diasAlVencimiento = (fechaDeVencimiento-fechaActual).TotalDays;
+                double tasaNeta = ((valorFacial - valorTransadoNeto) / (valorTransadoNeto * (diasAlVencimiento / 365))) * 100;
+                double tasaBruta = tasaNeta / (1 - tasaDeImpuesto);
+                double valorTransadoBruto = valorFacial / (1 + ((tasaBruta / 100) * (diasAlVencimiento / 365)));
+                impuesto = valorTransadoNeto - valorTransadoBruto;
+
+            }
+            else
+            {
+                impuesto = 0;
+            }
+            return Math.Round(impuesto,4);
+        }
+    }
+}
