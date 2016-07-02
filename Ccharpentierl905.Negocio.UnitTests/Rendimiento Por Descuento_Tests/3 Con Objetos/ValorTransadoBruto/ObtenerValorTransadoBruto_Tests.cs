619e9bad71ee17a813579b7a13d2d0b8a3e072c2
Chris <Chris@KAREN-PC>
2016-07-02 10:07:52 -0600

    aaa

--- Ccharpentierl905.Negocio.UnitTests/Rendimiento Por Descuento_Tests/3 Con Objetos/ValorTransadoBruto/ObtenerValorTransadoBruto_Tests.cs
+++ Ccharpentierl905.Negocio.UnitTests/Rendimiento Por Descuento_Tests/3 Con Objetos/ValorTransadoBruto/ObtenerValorTransadoBruto_Tests.cs
@@ -7,16 +7,15 @@ namespace ConObjetos_TESTS
     [TestClass]
     public class RendimientoPorDescuento_ValorTransadoBruto_Tests
     {
-        private object elResultadoEsperado;
+        private double elResultadoEsperado;
         private double elResultadoObtenido;
 
         [TestMethod]
         public void ValorTransadoBruto_ConTratamientoFiscal_Test()
         {
-            elResultadoEsperado = 298378.3784;
+            elResultadoEsperado = 298378.378378378;
             elResultadoObtenido = new ValorTransadoBruto(320000, 300000, 0.08, new DateTime(2016, 10, 10), new DateTime(2016, 3, 3)).ObtenerValorTransadoBruto();
-            elResultadoObtenido = Math.Round(elResultadoObtenido, 4);
-            Assert.AreEqual(elResultadoObtenido, elResultadoEsperado);
+            Assert.AreEqual(elResultadoObtenido, elResultadoEsperado,0.000000001);
         }
     }
 }
